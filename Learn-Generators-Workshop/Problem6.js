6b3b0099eb8e8609e57ba264a4f52eba04d6edb4
Sid <sid6327@gmail.com>
2017-02-19 11:07:09 +0530

    Adding solution to Problem6

--- Learn-Generators-Workshop/Problem6.js
+++ Learn-Generators-Workshop/Problem6.js
@@ -7,10 +7,45 @@ function askFoo() {
 function run(generator) {
   var it = generator();
   var p = it.next();
-  console.log(p.value.then(console.log));
+  it.next(p.value);
 }
 
 run(function* () {
   var foo = yield askFoo();
-  console.log(foo.then());
+  foo.then(console.log);
 })
+
+/*
+function getFoo () {
+  return new Promise(function (resolve, reject){
+    resolve('foo');
+  });
+}
+
+function run (generator) {
+  var it = generator();
+
+  function go(result) {
+    // take a look also on `Generator.prototype.return`
+    if (result.done) return result.value;
+
+    return result.value.then(function (value) {
+      return go(it.next(value));
+    }, function (error) {
+      return go(it.throw(error));
+    });
+
+  }
+
+  go(it.next());
+}
+
+run(function* () {
+  try {
+    var foo = yield getFoo();
+    console.log(foo);
+  } catch (e) {
+    console.log(e);
+  }
+});
+*/
